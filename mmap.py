e3caa67904fa16313d38422a930c311d3fa9d1b6
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-06-21 01:50:34 -0300

    Add files via upload

--- mmap.py
+++ mmap.py
@@ -1,37 +1,55 @@
+#Link de referência: https://realpython.com/python-mmap/
 import timeit
 import mmap
 
-print("Selecione alguma opção\n")
-entrada = int(input('Leitura mmap [1]\nEscrita mmap [2]\n->'))
-
-###Leitura mmap###
-
-if entrada == 1:
-    SETUP_CODE = '''import mmap'''
-
-
-    TEST_CODE = '''
-def mmap_io(filename):
+def mmap_io_read(filename):#Leitura mmap
     with open(filename, mode="r", encoding="utf8") as file_obj:
         with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
             text = mmap_obj.read()
-            
-mmap_io('996-0.txt')
-'''
-    resultadoMmapLeitura = timeit.repeat(setup = SETUP_CODE, stmt = TEST_CODE, repeat=3, number = 1)
-    print(resultadoMmapLeitura)
-    
-###Escrita mmap###
 
-#TEST_CODE2 =
-elif entrada == 2:
-    def mmap_io_write(filename, text):
-        with open(filename, mode="a", encoding="utf-8") as file_obj:
-            with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_WRITE) as mmap_obj:
-                mmap_obj.write(text)
+def regular_io_read(filename):#Leitura convencional
+    with open(filename, mode="r", encoding="utf8") as file_obj:
+        text = file_obj.read()
 
+def mmap_io_write(filename):#Escrita mmap
+    with open(filename, mode="r+") as file_obj:
+        with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_WRITE) as mmap_obj:
+            mmap_obj[10:16] = b"python"
+            mmap_obj.flush()
 
-    mmap_io_write('996-0.txt','1')
+def regular_io(filename):#Escrita convencional
+    with open(filename, mode="r+", encoding="utf8") as file_obj:
+        file_obj.write(b"python")
 
+files = ['8.txt','16.txt','32.txt','64.txt','128.txt','256.txt','512.txt']
 
-    resultadoMmapEscrita = timeit.repeat(setup = SETUP_CODE, stmt = TEST_CODE2, repeat=3, number = 1)
+print("Selecione alguma opção\n")
+entrada = int(input('Leitura [1]\nEscrita [2]\n->'))
+
+for filename in files:
+
+    if entrada == 1:#Leitura de arquivo
+        k = timeit.repeat(
+        "regular_io_read(filename)",
+        repeat=3,
+        number=1,
+        setup="from __main__ import regular_io_read, filename")
+
+        x = timeit.repeat(
+        "mmap_io_read(filename)",
+        repeat=3,
+        number=1,
+        setup="from __main__ import mmap_io_read, filename")
+
+        print(filename[:-4]+" MB")
+        print("Estratégia tradicional - Tempo médio:",sum(k)/len(k))
+        print("Estratégia com mmap: - Tempo médio:",sum(x)/len(x))
+        print()
+        
+    ###Escrita mmap###
+
+    #TEST_CODE2 =
+    elif entrada == 2:
+        
+        mmap_io_write('oi.txt')
+        resultadoMmapEscrita = timeit.repeat(setup = SETUP_CODE, stmt = TEST_CODE2, repeat=3, number = 1)

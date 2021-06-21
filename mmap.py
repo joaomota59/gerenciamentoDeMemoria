5a7d9ba8a20f4ce4629a666ee64a0c2606ecd391
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-06-20 23:24:10 -0300

    Add files via upload

---
+++ mmap.py
@@ -0,0 +1,10 @@
+import mmap
+
+def mmap_io(filename):
+    with open(filename, mode="r", encoding="utf8") as file_obj:
+        with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
+            text = mmap_obj.read()
+            print(text)
+
+            
+mmap_io('996-0.txt')

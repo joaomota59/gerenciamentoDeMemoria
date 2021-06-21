aa79a6a08012f0f77c08b2cbc19a777b1fe30d25
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-06-21 00:08:17 -0300

    add arquivo mmap leitura windows

--- mmap.py
+++ mmap.py
@@ -1,10 +1,37 @@
+import timeit
 import mmap
 
+print("Selecione alguma opção\n")
+entrada = int(input('Leitura mmap [1]\nEscrita mmap [2]\n->'))
+
+###Leitura mmap###
+
+if entrada == 1:
+    SETUP_CODE = '''import mmap'''
+
+
+    TEST_CODE = '''
 def mmap_io(filename):
     with open(filename, mode="r", encoding="utf8") as file_obj:
         with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
             text = mmap_obj.read()
-            print(text)
-
             
 mmap_io('996-0.txt')
+'''
+    resultadoMmapLeitura = timeit.repeat(setup = SETUP_CODE, stmt = TEST_CODE, repeat=3, number = 1)
+    print(resultadoMmapLeitura)
+    
+###Escrita mmap###
+
+#TEST_CODE2 =
+elif entrada == 2:
+    def mmap_io_write(filename, text):
+        with open(filename, mode="a", encoding="utf-8") as file_obj:
+            with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_WRITE) as mmap_obj:
+                mmap_obj.write(text)
+
+
+    mmap_io_write('996-0.txt','1')
+
+
+    resultadoMmapEscrita = timeit.repeat(setup = SETUP_CODE, stmt = TEST_CODE2, repeat=3, number = 1)
